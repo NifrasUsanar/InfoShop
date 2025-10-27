6f350e61700970e6e85e9137aed0af74d1c21888
Nifras Usanar <nifrasusanar@gmail.com>
2025-10-27 15:51:54 +0530

    Build fix

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -12,8 +12,6 @@ const appName = import.meta.env.VITE_APP_NAME || 'InfoShop';
 
 import { InertiaProgress } from '@inertiajs/progress';
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
-
 InertiaProgress.init({
   color:'#0a0a0a',
   includeCSS: true,
